8f5acc24981065a1d2061df9302edba9a66392c3
AldoRH <aldo.romeroh@gmail.com>
2024-02-12 23:46:59 -0600

    Create app_theme
    Tema global de la aplicación

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,71 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  static const Color primary = Colors.indigo;
+
+  static final ThemeData lightTheme = ThemeData.light().copyWith(
+        //Color primario
+        primaryColor: Colors.indigo,
+        //AppBar theme
+        appBarTheme: const AppBarTheme(
+          color: primary,
+          elevation: 0,
+          titleTextStyle: TextStyle( color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)
+        ),
+        
+        //TextButton Theme
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: primary,)
+        ),
+
+        //FloatingButtons
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primary
+        ),
+
+        //ElevatedButtons
+        elevatedButtonTheme:  ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo,
+            shape: const StadiumBorder(),
+            elevation: 0,
+          ),
+        ),
+
+        //Inputs
+        inputDecorationTheme: const InputDecorationTheme(
+          floatingLabelStyle: TextStyle(color: primary),
+
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(  color: primary),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) )
+          ),
+
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(  color: primary),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) ),
+        ),
+
+        border : OutlineInputBorder(
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) ),
+        )
+        )
+      );
+
+  static final ThemeData darkTheme = ThemeData.dark().copyWith(
+        //Color primario
+        primaryColor: Colors.indigo,
+        //AppBar theme
+        appBarTheme: const AppBarTheme( 
+          color: primary,
+          elevation: 0
+        ),
+
+        scaffoldBackgroundColor: Colors.black
+
+);
+
+}

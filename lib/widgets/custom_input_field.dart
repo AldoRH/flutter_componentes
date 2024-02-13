171bd39fd14074fe2960606e4e048cedd94d60a1
AldoRH <aldo.romeroh@gmail.com>
2024-02-12 23:48:38 -0600

    Create custom:input_field
    Widget que personaliza los inputs

---
+++ lib/widgets/custom_input_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CustominputField extends StatelessWidget {
+
+  final String? hintText;
+  final String? labelText;
+  final String? helperText;
+  final IconData? icon;
+  final IconData? suffixIcon;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  final String formProperty;
+  final Map<String, String> formValues;
+  
+
+  const CustominputField({
+    super.key,
+    this.hintText,
+    this.labelText,
+    this.helperText,  
+    this.icon,    
+    this.suffixIcon, 
+    this.keyboardType, 
+    this.obscureText = false, 
+    required this.formProperty, 
+    required this.formValues,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: false,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      initialValue: '',
+      textCapitalization: TextCapitalization.words,
+      onChanged: (value) => formValues[formProperty] = value,
+      validator: (value) {
+        if (value == null) return 'Este campo es necesario';
+        return value.length < 3 ? 'Minimo de 3 letras' : null;
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration:  InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        helperText: helperText,
+        //prefixIcon: Icon(Icons.verified_user_outlined),
+        suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
+        icon: icon == null ? null : Icon(icon),
+      ),
+    );
+  }
+}
